023cb1f992f95ebb208f8ed0b298ab2bab41aeca
Zaheer Sufi <zaheerjunaidsufi@gmail.com>
2023-09-09 20:35:15 -0500

    Finished remaining balance

--- fullstackPackage/app.js
+++ fullstackPackage/app.js
@@ -7,13 +7,25 @@ const budgetDay = {
 };
 const balanceDay = [0, 0, 0, 0];
 
-let remainingBalance = 30; //money starting off with
+let totalBalance = budgetDay.Personal + budgetDay.School + budgetDay.Food + budgetDay.Miscellaneous;
+
+let remainingBalance = totalBalance; //money starting off with
+
+let remPersonalBudget = budgetDay.Personal;
+let remSchoolBudget = budgetDay.School;
+let remFoodBudget = budgetDay.Food;
+let remMiscellaneousBudget = budgetDay.Miscellaneous;
 
 function updateBudgetDisplay() {
   document.getElementById("personalBudget").textContent = budgetDay.Personal.toFixed(2);
   document.getElementById("schoolBudget").textContent = budgetDay.School.toFixed(2);
   document.getElementById("foodBudget").textContent = budgetDay.Food.toFixed(2);
   document.getElementById("miscellaneousBudget").textContent = budgetDay.Miscellaneous.toFixed(2);
+  document.getElementById("totalBalance").textContent = totalBalance;
+  document.getElementById("remPersonalBudget").textContent = remPersonalBudget;
+  document.getElementById("remSchoolBudget").textContent = remSchoolBudget;
+  document.getElementById("remFoodBudget").textContent = remFoodBudget;
+  document.getElementById("remMiscellaneousBudget").textContent = remMiscellaneousBudget;
   document.getElementById("remainingBalance").textContent = remainingBalance;
 }
 
@@ -28,6 +40,24 @@ function spend() {
   
   balanceDay[Object.keys(budgetDay).indexOf(category)] += amount;
   remainingBalance -= amount;
+
+  if(category === 'Personal')
+  {
+    remPersonalBudget -= amount;
+  }
+  else if(category === "School")
+  {
+    remSchoolBudget -= amount;
+  }
+  else if(category === "Food")
+  {
+    remFoodBudget -= amount;
+  }
+  else if(category === "Miscellaneous")
+  {
+    remMiscellaneousBudget -= amount;
+  }
+
   const remainingBudget = budgetDay[category] - balanceDay[Object.keys(budgetDay).indexOf(category)];
 
   let resultMessage = "";
